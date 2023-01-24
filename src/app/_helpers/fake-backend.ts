1e2888182ffb6173ee7de84fe9f18a5182b89566
LuczaG <gergo.lucza@gmail.com>
2023-01-24 19:39:18 +0100

    Finished FakeBackend, rename environment

--- src/app/_helpers/fake-backend.ts
+++ src/app/_helpers/fake-backend.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
-import { delay, dematerialize, materialize, Observable, of, throwError } from 'rxjs';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, dematerialize, materialize } from 'rxjs/operators';
 
 // tömb a localStorage-ban regisztrált felhasználók számára
 const userKey = 'users';
@@ -12,7 +13,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
         const { url, method, headers, body } = request;
 
         return handleRout();
-            
+
         function handleRout() {
             switch (true) {
                 case url.endsWith('/users/authenticate') && method === 'POST':
@@ -24,15 +25,15 @@ export class FakeBackendInterceptor implements HttpInterceptor {
                 case url.match(/\/users\/\d+$/) && method === 'GET':
                     return getUserById();
                 case url.match(/\/users\/\d+$/) && method === 'PUT':
-                        return updateUser();
+                    return updateUser();
                 case url.match(/\/users\/\d+$/) && method === 'DELETE':
-                        return deleteUser();
-                default: 
+                    return deleteUser();
+                default:
                     // a fent nem kezelt kérések továbbítása.
                     return next.handle(request);
-            }  
-        } 
-        
+            }
+        }
+
         function authenticate() {
             const { username, password } = body;
             const user = users.find(x => x.usernaem === username && x.password === password);
@@ -50,14 +51,53 @@ export class FakeBackendInterceptor implements HttpInterceptor {
                 return error('Username "' + user.username + '" is already taken');
             }
 
-            user.id = users.length ? Math.max(...users.map(x => x.id)) +1 : 1;
+            user.id = users.length ? Math.max(...users.map(x => x.id)) + 1 : 1;
             users.push(user);
             localStorage.setItem(userKey, JSON.stringify(users));
             return ok();
         }
 
+        function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+
+            return ok(users.map(x => basicDetails(x)));
+        }
+
+        function getUserById() {
+            if (!isLoggedIn()) return unauthorized();
+
+            const user = users.find(x => x.id === idFromUrl());
+            return ok(basicDetails(user));
+        }
+
+        function updateUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            let params = body;
+            let user = users.find(x => x.id === idFromUrl());
+
+            // Csak akkor módosítjuk a jelszót, ha van benne érték.
+            if (params.password) {
+                delete params.password;
+            }
+
+            Object.assign(user, params);
+            localStorage.setItem(userKey, JSON.stringify(users));
+
+            return ok();
+        }
+
+        function deleteUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            users = users.filter(x => x.id !== idFromUrl());
+            localStorage.setItem(userKey, JSON.stringify(users));
+
+            return ok();
+        }
+
         function error(message: string) {
-            return throwError(() => ({ error: { message} })).pipe(
+            return throwError(() => ({ error: { message } })).pipe(
                 materialize(), delay(500), dematerialize()
                 // Hívjuk meg a materialize() és dematerialize() függvényeket.
                 // hogy hiba esetén is biztosítsk a késleltetést.
@@ -76,5 +116,27 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             const { id, username, firstName, lastName } = user;
             return { id, username, firstName, lastName };
         }
+
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
+
+        function unauthorized() {
+            return throwError(() => ({ status: 401, error: { message: 'Unauthorized' } }))
+                .pipe(materialize(), delay(500), dematerialize());
+        }
+
+        function idFromUrl() {
+            const urlParts = url.split('/'); // Ez lesz belőle -> ["users", "edit", "1"]
+            return parseInt(urlParts[urlParts.length - 1]);
+        }
     }
+}
+
+export const fakeBackendProvider = {
+    // Hamis backend-et használunk
+    // backend less developement
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
 }
